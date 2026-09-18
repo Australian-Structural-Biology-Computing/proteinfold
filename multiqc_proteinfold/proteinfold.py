746c72148521e306a48865a08568ec91927855f2
keiran.rowell <k.rowell@unsw.edu.au>
2026-09-18 15:00:37 +1000

    hasattr breaks with new pydandtic model

--- multiqc_proteinfold/proteinfold.py
+++ multiqc_proteinfold/proteinfold.py
@@ -54,14 +54,14 @@ class MultiqcModule(BaseMultiqcModule):
         )
 
         # Want to treat the ranked inference runs as 'sub-samples' for grouping logic, even if not separate files
-        if not hasattr(config, "table_sample_merge"):
-            config.table_sample_merge = {}
+        current_merge = getattr(config, "table_sample_merge", None) or {}
+        config.table_sample_merge = {**rank_merge, **current_merge}
 
         # Some codes generated 5 inferences for 5 models and all 25 are processed. If a user sets more they're an expert and can custom handle
         rank_merge = {f"rank_{i}": [f"_rank_{i}"] for i in range(25)}
 
         # Load user sample merge config in preference to rank_N default ranking, if user config exists
-        config.table_sample_merge = {**rank_merge, **getattr(config, "table_sample_merge", {})}
+        config.table_sample_merge = {**rank_merge, **current_merge}
 
         mode_dict = {
             "alphafold2": "AlphaFold2",
