5cb13119a1bab82875bc60caca613a742d40d588
jscgh <j.caley@unsw.edu.au>
2026-09-23 12:06:01 +1000

    Clean up stale MultiQC and Foldseek wiring

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='MultiQC plugin for proteinfold pipeline metric tsv outputs',
     url='https://github.com/nf-core/proteinfold',
     packages=["multiqc_proteinfold"],
-    package_data={"multiqc_proteinfold": ["*.yaml"]},
+    package_data={"multiqc_proteinfold": ["*.yml"]},
     include_package_data=True,
     entry_points={
         'multiqc.modules.v1': [
